4b54e97a434995df3c1c752c23229c8189d69c24
Daron Wiafe <daronkwiafe@gmail.com>
2022-09-05 19:17:45 +0200

    Fix linter errors

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-const menuButton = document.getElementById('menuButton');
 const header = document.getElementById('header');
 const menu = document.getElementById('menu');
-const menuClose = document.getElementById('menuClose');
 
 function openMenu() {
-    header.style.display = "none";
-    menu.style.display = "flex";
+  header.style.display = 'none';
+  menu.style.display = 'flex';
 }
 
 function closeMenu() {
-    header.style.display = "flex";
-    menu.style.display = "none";
+  header.style.display = 'flex';
+  menu.style.display = 'none';
 }
