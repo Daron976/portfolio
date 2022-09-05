7fea897c6a5a713680498ce6efd467d0b41c9076
Daron Wiafe <daronkwiafe@gmail.com>
2022-09-06 00:26:03 +0200

    Update linter.yml file and fix linter errors

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const header = document.getElementById('header');
 const menu = document.getElementById('menu');
+const menuButton = document.getElementById('menuButton');
+const cancelButton = document.getElementById('cancelButton');
+const mobileLinks = document.getElementsByClassName('mobile_links');
+const onClickOpen = document.createAttribute('onclick');
+const onClickClose = document.createAttribute('onclick');
 
 function openMenu() {
   header.style.display = 'none';
@@ -9,4 +14,14 @@ function openMenu() {
 function closeMenu() {
   header.style.display = 'flex';
   menu.style.display = 'none';
+}
+
+onClickOpen.value = openMenu();
+onClickClose.value = closeMenu();
+
+menuButton.setAttribute('onclick', 'openMenu()');
+cancelButton.setAttribute('onclick', 'closeMenu()');
+
+for (let i = 0; i < 3; i += 1) {
+  mobileLinks[i].setAttribute('onclick', 'closeMenu()');
 }
